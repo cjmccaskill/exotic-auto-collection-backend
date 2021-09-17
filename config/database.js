ab012fb3fa550387a4cc5a7af2306aa97e37f3ed
CJ McCaskill <cjmccaskill@gmail.com>
2021-09-16 22:09:25 -0500

    reconfig api

--- config/database.js
+++ config/database.js
@@ -1,20 +1,21 @@
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
-module.exports = () => ({
+module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
         ssl: {
-          rejectUnauthorized: false,
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
         },
       },
       options: {},
